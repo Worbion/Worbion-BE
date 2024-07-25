7fefdfebc23f21933cd1e076744978614576fbdd
Cemal <ctuysuz18@posta.pau.edu.tr>
2024-07-25 20:19:49 +0300

    The ClientChannelType enum was created to detect the request channel.

---
+++ Sources/App/Models/Header/ClientChannelType.swift
@@ -0,0 +1,20 @@
+//
+//  ClientChannelType.swift
+//
+//
+//  Created by Cemal on 25.07.2024.
+//
+
+import Foundation
+
+// MARK: - ClientChannelType
+enum ClientChannelType: Int {
+    case clientMobile = 0
+    case clientWeb = 1
+    case panelWeb = 2
+}
+
+// MARK: - ClientChannelType
+extension ClientChannelType: CustomHeaderKeyable {
+    static var customHeaderKey: String = "x-client-channel-type"
+}
