c7b94570d4be3965eed5d4112682984dbf209674
Cemal <ctuysuz18@posta.pau.edu.tr>
2024-07-25 21:26:44 +0300

    Create user from panel method response

---
+++ Sources/App/Models/DTO/Users/CreateUserFromPanel/CreateUserFromPanelResponse.swift
@@ -0,0 +1,13 @@
+//
+//  CreateUserFromPanelResponse.swift
+//
+//
+//  Created by Cemal on 22.07.2024.
+//
+
+import Vapor
+
+// MARK: - CreateUserFromPanelResponse
+struct CreateUserFromPanelResponse: Content {
+    var userId: Int64?
+}
