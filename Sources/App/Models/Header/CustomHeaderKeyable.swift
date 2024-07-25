d229813694069a4eed833cd15642aeb4369cc3d5
Cemal <ctuysuz18@posta.pau.edu.tr>
2024-07-25 20:17:58 +0300

    The CustomHeaderKeyable protocol was created to detect the keys of customized header fields.

---
+++ Sources/App/Models/Header/CustomHeaderKeyable.swift
@@ -0,0 +1,12 @@
+//
+//  CustomHeaderKeyable.swift
+//
+//
+//  Created by Cemal on 25.07.2024.
+//
+
+import Foundation
+
+protocol CustomHeaderKeyable {
+    static var customHeaderKey: String { get }
+}
