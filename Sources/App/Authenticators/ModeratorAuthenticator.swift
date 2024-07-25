bd1ca50cc9ac923335e9548fc8753d262a5c375d
Cemal <ctuysuz18@posta.pau.edu.tr>
2024-07-25 19:41:50 +0300

    Moderator Authenticator  was created for the parts that need to be accessed with at least the moderator level.

---
+++ Sources/App/Authenticators/ModeratorAuthenticator.swift
@@ -0,0 +1,27 @@
+//
+//  ModeratorAuthenticator.swift
+//
+//
+//  Created by Cemal on 20.07.2024.
+//
+
+import Vapor
+import JWT
+
+struct ModeratorAuthenticator: JWTAuthenticator {
+    typealias Payload = App.Payload
+    
+    func authenticate(jwt: Payload, for request: Request) -> EventLoopFuture<Void> {
+        guard
+            jwt.role.doCheckPermission(has: .moderator)
+        else {
+            let error = Abort(.unauthorized)
+            return request.eventLoop.makeFailedFuture(error)
+        }
+        request.auth.login(jwt)
+        return request.eventLoop.makeSucceededFuture(())
+    }
+}
+
+
+
