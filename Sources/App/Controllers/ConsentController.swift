af7e5625ee8b2bccf7f10c7bd5f4aa4baadc6a0a
Cemal <ctuysuz18@posta.pau.edu.tr>
2024-07-29 14:08:21 +0300

    Consent API methods has been implemented and registered at routes

--- Sources/App/Controllers/ConsentController.swift
+++ Sources/App/Controllers/ConsentController.swift
@@ -8,87 +8,78 @@
 import Vapor
 import Fluent
 
+// MARK: - ConsentController
 struct ConsentController: RouteCollection {
     func boot(routes: any RoutesBuilder) throws {
-        routes.group("consent") { user in
-            user.group(AdminAuthenticator()) { authenticated in
-                authenticated.post(use: createConsent(request:))
-                authenticated.put(":id", use: updateConsent(request:))
+        routes.group("consents") { consents in
+            consents.group(AdminAuthenticator()) { consents in
+                // Fetch All Consents
+                consents.get(use: getAllConsents(request:))
+                // Create a consent
+                consents.post(use: createConsent(request:))
+                // Update a Consent
+                consents.put(":id", use: updateConsent(request:))
+                
+                consents.group(":id", "versions") { version in
+                    // Create Consent Version
+                    version.post(use: createConsentVersion(request:))
+                    // Get all versions of consent
+                    /// consents/{consent_id}/versions -> get all consents
+                    /// consents/{consent_id}/versions?version = 1.0 -> get just spesific version
+                    version.get(use: getConsentVersions(request:))
+                }
+            }
+            
+            consents.group(UserAuthenticator()) { consents in
+                // get latest version of consent by type
+                /// consents?type=privacy_policy/latest-version
+                consents.get("latest-version", use: getLastestVersionOfConsent(request:))
+                // get all user visible consent types
+                consents.get("types", use: getAllUserVisibleConsentTypes(request:))
             }
         }
     }
 }
 
+// MARK: - Consent Operations
 private extension ConsentController {
     @Sendable
-    func createConsent(request: Request) async throws -> BaseResponse<ConsentCreateResponse> {
+    func createConsent(request: Request) async throws -> BaseResponse<Int64> {
         
-        try ConsentCreateRequest.validate(query: request)
+        try ConsentCreateRequest.validate(content: request)
         
         let consentCreateRequest = try request.content.decodeRequestContent(content: ConsentCreateRequest.self)
         
-        // Is there exist version of consent
-        let existConsent = try await ConsentEntity.query(on: request.db)
-            .filter(\.$consentVersion == consentCreateRequest.consentVersion)
-            .filter(\.$consentType == .enumCase(consentCreateRequest.consentType.rawValue))
-            .first()
-        
-        guard
-            existConsent == nil
-        else {
-            let message = "this content version of related consent type is already exist."
-            let error = GeneralError.generic(
-                userMessage: message,
-                systemMessage: message,
-                status: .conflict
-            )
+        let newConsent = ConsentEntity(create: consentCreateRequest)
+        do {
+            try await newConsent.save(on: request.db)
+        }catch {
+            print(error)
+            if let dbError = error as? DatabaseError, dbError.isConstraintFailure {
+                let message = "This consent type has already a consent. You cant create again. Please create a new version for this consent."
+                let error = GeneralError.generic(userMessage: message, systemMessage: message, status: .conflict)
+                throw error
+            }
             throw error
         }
-        
-        let fileName = "consents/\(consentCreateRequest.consentName)/\(consentCreateRequest.consentVersion).pdf"
-        
-        let uploadConsentFileResult = try await request.storageHelper.uploadFile(
-            fileData: consentCreateRequest.consentFile,
-            contentType: .pdf,
-            fileName: fileName
-        )
-        
-        guard 
-            let pdfLink = uploadConsentFileResult.mediaLink
-        else {
-            let message = "An Error occured while uploading pdf."
-            let error = GeneralError.generic(
-                userMessage: message,
-                systemMessage: message,
-                status: .internalServerError
-            )
-            throw error
-        }
-        
-        let newConsent = ConsentEntity(
-            create: consentCreateRequest,
-            consent: pdfLink
-        )
-        
-        try await newConsent.save(on: request.db)
-        return .success(data: .init(consentId: newConsent.id))
+        let newConsentId = try newConsent.requireID()
+        return .success(data: newConsentId)
     }
     
     @Sendable
     func updateConsent(request: Request) async throws -> BaseResponse<Bool> {
         guard
-            let consentIdParameter = request.parameters.get("id"),
-            let consentId = Int64(consentIdParameter)
+            let consentId = request.parameters.get("id", as: Int64.self)
         else {
             let error = GeneralError.generic(
                 userMessage: nil,
-                systemMessage: "UserId is missing or incorrect parameter.",
+                systemMessage: "ConsentId is missing or incorrect parameter.",
                 status: .badRequest
             )
             throw error
         }
         
-        try ConsentUpdateRequest.validate(query: request)
+        try ConsentUpdateRequest.validate(content: request)
         
         let consentUpdateRequest = try request.content.decodeRequestContent(content: ConsentUpdateRequest.self)
         
@@ -119,5 +110,160 @@ private extension ConsentController {
         try await consentEntity.update(on: request.db)
         
         return .success(data: true)
+    }
+    
+    @Sendable
+    func getAllConsents(request: Request) async throws -> BaseResponse<[ConsentEntity]> {
+        let consents = try await ConsentEntity.query(on: request.db).all()
+        return .success(data: consents)
+    }
+    
+    @Sendable
+    func getAllUserVisibleConsentTypes(request: Request) async throws -> BaseResponse<[ConsentType]> {
+        let consents = ConsentType.allCases.filter { $0.isUserVisibleAgreementType }
+        return .success(data: consents)
+    }
+}
+
+// MARK: - Consent Version Operations
+private extension ConsentController {
+    @Sendable
+    func createConsentVersion(request: Request) async throws -> BaseResponse<Int64> {
+        // Get ConsentId
+        guard
+            let consentId = request.parameters.get("id", as: Int64.self)
+        else {
+            let error = GeneralError.generic(
+                userMessage: nil,
+                systemMessage: "ConsentId is missing or incorrect parameter.",
+                status: .badRequest
+            )
+            throw error
+        }
+        
+        try CreateConsentVersionRequest.validate(content: request)
+        
+        let createConsentVersionRequest = try request.content.decodeRequestContent(content: CreateConsentVersionRequest.self)
+        
+        // Fetch consent
+        guard 
+            let _ = try await ConsentEntity.find(consentId, on: request.db)
+        else {
+            let message = "Consent not found"
+            let error = GeneralError.generic(
+                userMessage: message,
+                systemMessage: message,
+                status: .notFound
+            )
+            throw error
+        }
+        
+        // upload new consent into the storage
+        let uuid = UUID().uuidString
+        let fileName = "consents/\(consentId)/\(uuid)-\(createConsentVersionRequest.version).pdf"
+        
+        let fileUploadRequest = try await request.storageHelper.uploadFile(
+            fileData: createConsentVersionRequest.consentFile,
+            contentType: .pdf,
+            fileName: fileName
+        )
+        
+        guard
+            let pdfLink = fileUploadRequest.mediaLink
+        else {
+            let userMessage = "An error occured while uploading consent file"
+            let systemMessage = "media link is nil"
+            
+            let error = GeneralError.generic(
+                userMessage: userMessage,
+                systemMessage: systemMessage,
+                status: .internalServerError
+            )
+            throw error
+        }
+        
+        let versionEntity = ConsentVersionEntity(
+            create: createConsentVersionRequest,
+            consentId: consentId,
+            url: pdfLink
+        )
+        
+        do {
+            try await versionEntity?.save(on: request.db)
+        }catch {
+            if let dbError = error as? DatabaseError, dbError.isConstraintFailure {
+                let message = "This consent version is already exist. Please change the version."
+                let error = GeneralError.generic(userMessage: message, systemMessage: message, status: .conflict)
+                throw error
+            }
+            throw error
+        }
+        let newVersionId = try versionEntity?.requireID()
+        
+        return .success(data: newVersionId)
+    }
+    
+    @Sendable
+    func getLastestVersionOfConsent(request: Request) async throws -> BaseResponse<ConsentVersionResponse> {
+        let consentType = try request.query.get(
+            decodableType: ConsentType.self,
+            at: "type"
+        )
+        
+        let latestVersionOfConsent = try await ConsentVersionEntity.query(on: request.db)
+            .join(parent: \ConsentVersionEntity.$consent)
+            .filter(ConsentEntity.self, \.$consentType == consentType)
+            .sort(\ConsentVersionEntity.$version, .descending)
+            .first()
+
+        guard
+            let latestVersionOfConsent
+        else {
+            let message = "Consent version not found."
+            let error = GeneralError.generic(
+                userMessage: message,
+                systemMessage: message,
+                status: .notFound
+            )
+            throw error
+        }
+        
+        let response = ConsentVersionResponse(
+            consentUrl: latestVersionOfConsent.url,
+            version: latestVersionOfConsent.version
+        )
+        
+        return .success(data: response)
+    }
+    
+    @Sendable
+    func getConsentVersions(request: Request) async throws -> BaseResponse<[ConsentVersionXLResponse]> {
+        guard
+            let consentId = request.parameters.get("id", as: Int64.self)
+        else {
+            let error = GeneralError.generic(
+                userMessage: nil,
+                systemMessage: "ConsentId is missing or incorrect parameter.",
+                status: .badRequest
+            )
+            throw error
+        }
+        
+        var versionQuery = ConsentVersionEntity.query(on: request.db)
+            .filter(\.$consent.$id == consentId)
+        
+        let requestedVersion = try? request.query.get(
+            decodableType: Double.self,
+            at: "version"
+        )
+        
+        if let requestedVersion {
+            versionQuery.filter(\.$version == requestedVersion)
+        }
+        
+        let versions = try await versionQuery.all()
+        
+        let response = versions.compactMap { $0.mapConsentVersionXLResponse }
+        return .success(data: response)
     }
 }
