14aab44b4b7e595606f15c2e41f501fbc401a81b
Cemal <ctuysuz18@posta.pau.edu.tr>
2024-07-30 13:14:24 +0300

    DevieOS type

---
+++ Sources/App/Models/Entities/Device/DeviceOSType.swift
@@ -0,0 +1,14 @@
+//
+//  DeviceOSType.swift
+//
+//
+//  Created by Cemal on 29.07.2024.
+//
+
+import Vapor
+
+// MARK: - DeviceOSType
+enum DeviceOSType: String, Content {
+    case iOS = "androidOS"
+    case android = "iOS"
+}
