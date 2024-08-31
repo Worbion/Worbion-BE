1ddcee991c426533156f08d40cd4fa38264f1d02
Cemal <ctuysuz18@posta.pau.edu.tr>
2024-08-31 16:01:43 +0300

    localization DIR and default localization key has been implemented into Constants

---
+++ Sources/App/Constants/Constants+Language.swift
@@ -0,0 +1,14 @@
+//
+//  Constants+Language.swift
+//
+//
+//  Created by Cemal on 31.08.2024.
+//
+
+import Foundation
+
+// MARK: - Constants+Language
+extension Constants.Language {
+    static let defaultLocalizationLangKey: String = "tr"
+    static let localizationsDIR: String = "Localizations"
+}
