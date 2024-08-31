3e280b08547d1be0d6e2a14d43b4b560d1485207
Cemal <ctuysuz18@posta.pau.edu.tr>
2024-08-31 16:47:53 +0300

    Auth errors has localized

--- Sources/App/Errors/AuthenticationError.swift
+++ Sources/App/Errors/AuthenticationError.swift
@@ -71,7 +71,7 @@ extension AuthenticationError {
         case .invalidEmailOrPassword:
             return "auth.invalid_email_or_password"
         case .invalidEmail:
-            return "auth.email_invalid_email"
+            return "auth.invalid_email"
         case .refreshTokenOrUserNotFound:
             return "auth.refresh_token_or_user_not_found"
         case .refreshTokenHasExpired:
@@ -135,6 +135,25 @@ extension AuthenticationError {
 // MARK: - Error User Message
 extension AuthenticationError {
     var userMessage: String? {
-        return nil
+        switch self {
+        case .passwordsDontMatch:
+            return "passwords_dont_match"
+        case .emailAlreadyExists:
+            return "email_already_exists"
+        case .invalidEmailOrPassword:
+            return "invalid_email_or_password"
+        case .invalidEmail:
+            return "invalid_email"
+        case .userNotFound:
+            return "user_not_found"
+        case .emailTokenHasExpired:
+            return "email_token_has_expired"
+        case .emailIsNotVerified:
+            return "email_is_not_verified"
+        case .passwordTokenHasExpired:
+            return "password_token_has_expired"
+        default:
+            return nil
+        }
     }
 }
